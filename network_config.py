eae70b3d1ba7074942a74b21e4998d0579580629
Lodrien <amdyes77@gmail.com>
2025-04-08 14:26:40 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -2,16 +2,30 @@
 
 networks = {
     'Arbitrum': {
-        'rpc_url': 'https://sepolia.arbitrum.io', 
-        'chain_id': 421614,  
-        'contract_address': 
-        '0x22B65d0B9b59af4D3Ed59F18b9Ad53f5F4908B54'  
+        'rpc_url': 'https://sepolia.arbitrum.io',
+        'chain_id': 421614,
+        'contract_address':
+        '0x22B65d0B9b59af4D3Ed59F18b9Ad53f5F4908B54'
     },
     'OP Sepolia': {
         'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
         'chain_id': 11155420,
-        'contract_address': 
+        'contract_address':
         '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     },
+    'Base Sepolia': {
+        'rpc_url': 'https://base-sepolia-rpc.publicnode.com',
+        'chain_id': 84532,
+        'contract_address': '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3'
+    },
+    'Blast Sepolia': {
+        'rpc_url': 'https://sepolia.blast.io',
+        'chain_id': 168587773,
+        'contract_address': '0x9f3871ED60E0C611b751ca2D9F4DCfE1a719A9eB'
+    },
+    'Unichain Sepolia': {
+        'rpc_url': 'https://unichain-sepolia.drpc.org',
+        'chain_id': 1301,
+        'contract_address': '0x1cEAb5967E5f078Fa0FEC3DFfD0394Af1fEeBCC9'
+    }
 }
-
